36392aa3790d37a1ff18bbfca48979b26d7fe5dc
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2019-03-01 15:30:39 -0500

    plots

--- exp2_plot2.py
+++ exp2_plot2.py
@@ -8,14 +8,15 @@ V = {}
 Ib = {}
 Ie = {}
 Ic = {}
+Icmod = {}
+
+Is = 3.343531794569888e-14
+Ut = 0.028064113795844548
 
 for val in res:
     with open('trans_exp2_%s.csv' % val) as f:
         c = csv.reader(f, delimiter=",")
         next(c)
-        for i in range(0,600):
-            next(c)
-            
         R = int(val[:-1])*1000
         print(R)
         Vtmp = []
@@ -26,23 +27,27 @@ for val in res:
             Ibtmp += [float(row[1])]
             Ietmp += [-float(row[2])]
         Ictmp = np.array(Ietmp) - np.array(Ibtmp)
+        Icmodel = Is*np.exp((np.array(Vtmp)-R*np.array(Ietmp))/Ut)
         V[val] = Vtmp
         Ib[val] = Ibtmp
         Ie[val] = Ietmp
         Ic[val] = Ictmp
+        Icmod[val] = Icmodel
 
         
-fig = plt.figure()
-ax = plt.subplot(111)
+fig1k = plt.figure()
+i = 1
 
 for val in res:
+    ax = plt.subplot(3,1,i)
 
-    ax.semilogy(V[val],Ic[val], label=val)
+    ax.semilogy(V[val],Ic[val])
+    ax.semilogy(V[val],Icmod[val])
+    #ax.plot(V[val],Ie[val])
+    i += 1
     plt.xlabel('Base Voltage (V)')
     plt.ylabel('Collector Current (A)')
-    #plt.title(val + " Resistor")
+    plt.title(val + " Resistor")
 
-plt.title("Collector Current vs Base Voltage")
-plt.legend()
 #ax.semilogx(x,y)
 plt.show()
