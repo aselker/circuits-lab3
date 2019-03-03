13bfbb792104f0519cfad2756be0baec8dfca32a
Adam Selker <adamselker@gmail.com>
2019-03-03 14:41:39 -0500

    Switch from curve_fit to minimize

--- exp2_theoretical.py
+++ exp2_theoretical.py
@@ -17,7 +17,7 @@ for i in range(3):
     with open('trans_exp2_%s.csv' % rnames[i]) as f:
         c = csv.reader(f, delimiter=",")
         next(c)
-        for _ in range(800): # Throw away bad data - 600 for final, 800 for easier fit
+        for _ in range(700): # Throw away bad data - 600 for final, 800 for easier fit
           next(c)
         for row in c:
           vb_exp[i] += [float(row[0])]
@@ -58,37 +58,71 @@ for j in range(3):
   ie_t[j] = [ie_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
   ic_t[j] = [ic_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
 
+"""
+# These should not be used again
+Ut = np.nan
+Is = np.nan
+β = np.nan
+r = np.nan
+"""
+
 Ut_c, Is_c, β_c, r_c = [], [], [], []
 ic_c = [[],[],[]]
 for j in range(3):
 
   def ic_w(Vb, Ut, Is, α, r):
     ic = -Is + (Ut * α * lambertw(np.exp((Is*r+Vb*α)/(Ut*α))*Is*r/(Ut*α)))/r
-    # if (ic.imag != 0).any():
-    #   print("Nonzero imaginary part with params ", Ut, Is, α, r)
+    if (ic.imag != 0).any():
+      print("Nonzero imaginary part with params ", Ut, Is, α, r)
     return ic.real
 
   def ic_f(Vb, Ut, Is, β, r): 
     return ic_w(Vb, Ut, Is, β/(1+β), r)
 
-  # params = curve_fit(lambda Vb, Ut, Is, β, r: np.log(ic_f(Vb, Ut, Is, β, r)), vb_exp[j], np.log(ic_exp[j]), bounds = (0, np.inf))[0]
-  # params = curve_fit(ic_f, vb_exp[j], ic_exp[j], bounds = (1e-10, [1, 1, np.inf, np.inf]))[0]
-  params = curve_fit(ic_f, vb_exp[j], ic_exp[j], p0 = [0.028, 3.343e-14, 177, 10000], bounds = ([1e-4, 1e-18, 1, 100], [1, 1e-4, 1e5, 1e8]))[0]
-  # params = curve_fit(ic_f, vb_exp[j], ic_exp[j], maxfev = 1000000)[0]
+  """
+  params = curve_fit(lambda Vb, Ut, Is, β, r: np.log(ic_f(Vb, Ut, Is, β, r)), vb_exp[j], np.log(ic_exp[j]), p0 = [0.028, 3.343e-14, 177, 10000], bounds = (1e-18, [1, 1, np.inf, np.inf]))[0]
+  Ut_c += [params[0]]
+  Is_c += [params[1]]
+  β_c += [params[2]]
+  r_c += [params[3]]
+  """
+
+  """
+  params = curve_fit(lambda Vb, Ut, β, r: np.log(ic_f(Vb, Ut, Is, β, r)), vb_exp[j], np.log(ic_exp[j]), p0 = [0.028, 177, 10000], bounds = (1e-18, [1, np.inf, np.inf]))[0]
+  Ut_c += [params[0]]
+  Is_c += [Is]
+  β_c += [params[1]]
+  r_c += [params[2]]
+
   print(params)
+
+  ic_c[j] = [ic_f(Vb, Ut_c[j], Is_c[j], β_c[j], r_c[j]) for Vb in vb_exp[j]]
+  err = err_f(ic_c[j])
+  print("log rms error = ", err)
+  """
+
+  def err_f(ic): return np.mean(np.power(np.log(ic) - np.log(ic_exp[j]), 2))
+
+  res = minimize(lambda args: err_f([ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]), x0 = [0.028, 3.343e-14, 177, 10000], method='Nelder-Mead') #, bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
+  # res = minimize(lambda args: err_f([ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]), x0 = [0.028, 3.343e-14, 177, 10000], bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
+  print(res)
+  params = res.x
   Ut_c += [params[0]]
   Is_c += [params[1]]
   β_c += [params[2]]
   r_c += [params[3]]
 
   ic_c[j] = [ic_f(Vb, Ut_c[j], Is_c[j], β_c[j], r_c[j]) for Vb in vb_exp[j]]
+  err = err_f(ic_c[j])
+  print("log ms error = ", err)
+
 
 fig = plt.figure()
 ax = plt.subplot(111)
 
 # Joined semilog V-I plot
 for i in range(3):
-  ax.plot(vb_exp[i], ic_exp[i], ['r.', 'g.', 'b.'][i], label="Measured collector current (" + rnames[i] + " Ω)")
+  ax.semilogy(vb_exp[i], ic_exp[i], ['r.', 'g.', 'b.'][i], label="Measured collector current (" + rnames[i] + " Ω)")
   ax.plot(vb_exp[i], ic_c[i], ['k--', 'k-.', 'k:'][i], label="Theoretical collector current (" + rnames[i] + "Ω)")
 
 plt.title("Emitter-Degenerated Collector Current")
