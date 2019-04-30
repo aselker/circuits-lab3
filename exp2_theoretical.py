f7aa3aa1f0013703abe65236363ad09f8cdf85dc
Adam Selker <adamselker@gmail.com>
2019-04-30 18:13:53 -0400

    add a todo, and format

--- exp2_theoretical.py
+++ exp2_theoretical.py
@@ -7,56 +7,63 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize, curve_fit
 from scipy.special import lambertw
 
-vb_exp = [[],[],[]]
-ib_exp = [[],[],[]]
-ie_exp = [[],[],[]]
+# TODO: Add the un-degenerated transistor here
 
-rnames = ['1k','10k','100k']
+vb_exp = [[], [], []]
+ib_exp = [[], [], []]
+ie_exp = [[], [], []]
+
+rnames = ["1k", "10k", "100k"]
 
 for i in range(3):
-    with open('trans_exp2_%s.csv' % rnames[i]) as f:
+    with open("trans_exp2_%s.csv" % rnames[i]) as f:
         c = csv.reader(f, delimiter=",")
         next(c)
-        for _ in range(670): # Throw away bad data - 670 is min?
-          next(c)
+        for _ in range(670):  # Throw away bad data - 670 is min?
+            next(c)
         for row in c:
-          vb_exp[i] += [float(row[0])]
-          ib_exp[i] += [float(row[1])]
-          ie_exp[i] += [-float(row[2])]
+            vb_exp[i] += [float(row[0])]
+            ib_exp[i] += [float(row[1])]
+            ie_exp[i] += [-float(row[2])]
 
 
 ic_exp = np.array(ie_exp) - np.array(ib_exp)
 
 
-ve_t = [[],[],[]]
-ie_t = [[],[],[]]
-ic_t = [[],[],[]]
+ve_t = [[], [], []]
+ie_t = [[], [], []]
+ic_t = [[], [], []]
 for j in range(3):
 
-  r = [1000, 10000, 100000][j]
+    r = [1000, 10000, 100000][j]
 
-  # Values from experiment 1
-  Ut = 0.0280641
-  Is = 3.34353e-14
-  β = 177.098
+    # Values from experiment 1
+    Ut = 0.0280641
+    Is = 3.34353e-14
+    β = 177.098
 
-  def ic_f(Vbe): return Is * (np.exp(Vbe/Ut) - 1)
-  def ib_f(Vbe): return (Is/β) * (np.exp(Vbe/Ut) - 1)
-  def ie_f(Vbe): return ic_f(Vbe) + ib_f(Vbe)
+    def ic_f(Vbe):
+        return Is * (np.exp(Vbe / Ut) - 1)
 
-  def ir_f(Ve):
-    return Ve / r
+    def ib_f(Vbe):
+        return (Is / β) * (np.exp(Vbe / Ut) - 1)
 
-  def err(Vb, Ve): # Error is 0 iff KCL holds
-    i1 = ie_f(Vb - Ve)
-    i2 = ir_f(Ve)
-    return abs(i1 - i2)
+    def ie_f(Vbe):
+        return ic_f(Vbe) + ib_f(Vbe)
 
-  for vb in vb_exp[j]:
-    ve_t[j] += [minimize(lambda Ve: err(vb, Ve), 0, method='Nelder-Mead').x[0]]
+    def ir_f(Ve):
+        return Ve / r
 
-  ie_t[j] = [ie_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
-  ic_t[j] = [ic_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
+    def err(Vb, Ve):  # Error is 0 iff KCL holds
+        i1 = ie_f(Vb - Ve)
+        i2 = ir_f(Ve)
+        return abs(i1 - i2)
+
+    for vb in vb_exp[j]:
+        ve_t[j] += [minimize(lambda Ve: err(vb, Ve), 0, method="Nelder-Mead").x[0]]
+
+    ie_t[j] = [ie_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
+    ic_t[j] = [ic_f(vbe[0] - vbe[1]) for vbe in zip(vb_exp[j], ve_t[j])]
 
 """
 # These should not be used again
@@ -67,20 +74,28 @@ r = np.nan
 """
 
 Ut_c, Is_c, β_c, r_c = [], [], [], []
-ic_c = [[],[],[]]
-ib_c = [[],[],[]]
+ic_c = [[], [], []]
+ib_c = [[], [], []]
 for j in range(3):
 
-  def ic_w(Vb, Ut, Is, α, r):
-    ic = -Is + (Ut * α * lambertw(np.exp((Is*r+Vb*α)/(Ut*α))*Is*r/(Ut*α)))/r
-    # if (ic.imag != 0).any():
-    #  print("Nonzero imaginary part with params ", Ut, Is, α, r)
-    return ic.real
+    def ic_w(Vb, Ut, Is, α, r):
+        ic = (
+            -Is
+            + (
+                Ut
+                * α
+                * lambertw(np.exp((Is * r + Vb * α) / (Ut * α)) * Is * r / (Ut * α))
+            )
+            / r
+        )
+        # if (ic.imag != 0).any():
+        #  print("Nonzero imaginary part with params ", Ut, Is, α, r)
+        return ic.real
 
-  def ic_f(Vb, Ut, Is, β, r): 
-    return ic_w(Vb, Ut, Is, β/(1+β), r)
+    def ic_f(Vb, Ut, Is, β, r):
+        return ic_w(Vb, Ut, Is, β / (1 + β), r)
 
-  """
+    """
   params = curve_fit(lambda Vb, Ut, Is, β, r: np.log(ic_f(Vb, Ut, Is, β, r)), vb_exp[j], np.log(ic_exp[j]), p0 = [0.028, 3.343e-14, 177, 10000], bounds = (1e-18, [1, 1, np.inf, np.inf]))[0]
   Ut_c += [params[0]]
   Is_c += [params[1]]
@@ -88,7 +103,7 @@ for j in range(3):
   r_c += [params[3]]
   """
 
-  """
+    """
   params = curve_fit(lambda Vb, Ut, β, r: np.log(ic_f(Vb, Ut, Is, β, r)), vb_exp[j], np.log(ic_exp[j]), p0 = [0.028, 177, 10000], bounds = (1e-18, [1, np.inf, np.inf]))[0]
   Ut_c += [params[0]]
   Is_c += [Is]
@@ -102,31 +117,38 @@ for j in range(3):
   print("log rms error = ", err)
   """
 
-  def err_f(ic): return np.mean(np.power(np.log(ic) - np.log(ic_exp[j]), 2))
+    def err_f(ic):
+        return np.mean(np.power(np.log(ic) - np.log(ic_exp[j]), 2))
 
-  res = minimize(lambda args: err_f([ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]), x0 = [0.028, 3.343e-14, 177, 10000], method='Nelder-Mead') #, bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
-  # res = minimize(lambda args: err_f([ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]), x0 = [0.028, 3.343e-14, 177, 10000], bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
-  print(res)
-  params = res.x
-  Ut_c += [params[0]]
-  Is_c += [params[1]]
-  β_c += [params[2]]
-  r_c += [params[3]]
+    res = minimize(
+        lambda args: err_f(
+            [ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]
+        ),
+        x0=[0.028, 3.343e-14, 177, 10000],
+        method="Nelder-Mead",
+    )  # , bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
+    # res = minimize(lambda args: err_f([ic_f(Vb, args[0], args[1], args[2], args[3]) for Vb in vb_exp[j]]), x0 = [0.028, 3.343e-14, 177, 10000], bounds=[(0.015, 1), (1e-18, 1), (1e-18, np.inf), (1e-18, np.inf)], method='TNC')
+    print(res)
+    params = res.x
+    Ut_c += [params[0]]
+    Is_c += [params[1]]
+    β_c += [params[2]]
+    r_c += [params[3]]
 
-  ic_c[j] = [ic_f(Vb, Ut_c[j], Is_c[j], β_c[j], r_c[j]) for Vb in vb_exp[j]]
-  err = err_f(ic_c[j])
-  print("log ms error = ", err)
+    ic_c[j] = [ic_f(Vb, Ut_c[j], Is_c[j], β_c[j], r_c[j]) for Vb in vb_exp[j]]
+    err = err_f(ic_c[j])
+    print("log ms error = ", err)
 
-  ib_c[j] = [ic/(β_c[j]) for ic in ic_c[j]]
+    ib_c[j] = [ic / (β_c[j]) for ic in ic_c[j]]
 
 
 # Find incremental resistances
-rb_exp, rb_c, gm_exp, gm_c = [[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]]
+rb_exp, rb_c, gm_exp, gm_c = [[], [], []], [[], [], []], [[], [], []], [[], [], []]
 for j in range(3):
-  rb_exp[j] = np.diff(vb_exp[j]) / np.diff(ib_exp[j])
-  rb_c[j] = np.diff(vb_exp[j]) / np.diff(ib_c[j])
-  gm_exp[j] = np.diff(ic_exp[j]) / np.diff(vb_exp[j])
-  gm_c[j] = np.diff(ic_c[j]) / np.diff(vb_exp[j])
+    rb_exp[j] = np.diff(vb_exp[j]) / np.diff(ib_exp[j])
+    rb_c[j] = np.diff(vb_exp[j]) / np.diff(ib_c[j])
+    gm_exp[j] = np.diff(ic_exp[j]) / np.diff(vb_exp[j])
+    gm_c[j] = np.diff(ic_c[j]) / np.diff(vb_exp[j])
 
 
 fig = plt.figure()
@@ -134,8 +156,18 @@ ax = plt.subplot(111)
 
 # Joined semilog V-I plot
 for i in range(3):
-  ax.semilogy(vb_exp[i], ic_exp[i], ['r.', 'g.', 'b.'][i], label="Measured collector current (" + rnames[i] + " Ω)")
-  ax.plot(vb_exp[i], ic_c[i], ['k--', 'k-.', 'k-'][i], label="Theoretical collector current (" + rnames[i] + "Ω)")
+    ax.semilogy(
+        vb_exp[i],
+        ic_exp[i],
+        ["r.", "g.", "b."][i],
+        label="Measured collector current (" + rnames[i] + " Ω)",
+    )
+    ax.plot(
+        vb_exp[i],
+        ic_c[i],
+        ["k--", "k-.", "k-"][i],
+        label="Theoretical collector current (" + rnames[i] + "Ω)",
+    )
 
 plt.title("Emitter-Degenerated Collector Current")
 plt.xlabel("Base voltage (V)")
@@ -149,21 +181,38 @@ ax.cla()
 
 # Separate linear V-I plots
 for i in range(3):
-  ax.plot(vb_exp[i], ic_exp[i], ['r.', 'g.', 'b.'][i], label="Measured collector current")
-  ax.plot(vb_exp[i], ic_c[i], ['k--', 'k-.', 'k-'][i], label="Theoretical collector current")
-  plt.title("Emitter-Degenerated Collector Current (" + rnames[i] + " Ω)")
-  plt.xlabel("Base voltage (V)")
-  plt.ylabel("Collector current (A)")
-  plt.grid(True)
-  ax.legend()
-  plt.savefig("exp2_ic_" + rnames[i] + ".pdf")
-  ax.cla()
+    ax.plot(
+        vb_exp[i], ic_exp[i], ["r.", "g.", "b."][i], label="Measured collector current"
+    )
+    ax.plot(
+        vb_exp[i],
+        ic_c[i],
+        ["k--", "k-.", "k-"][i],
+        label="Theoretical collector current",
+    )
+    plt.title("Emitter-Degenerated Collector Current (" + rnames[i] + " Ω)")
+    plt.xlabel("Base voltage (V)")
+    plt.ylabel("Collector current (A)")
+    plt.grid(True)
+    ax.legend()
+    plt.savefig("exp2_ic_" + rnames[i] + ".pdf")
+    ax.cla()
 
 
 # Joined log-log inc. res. plot
 for i in range(3):
-  ax.loglog(ib_exp[i][100:-1], rb_exp[i][100:], ['r.', 'g.', 'b.'][i], label="Measured incremental base resistance (" + rnames[i] + " Ω)")
-  ax.plot(ib_exp[i][100:-1], rb_c[i][100:], ['k--', 'k-.', 'k-'][i], label="Theoretical incremental base resistance (" + rnames[i] + "Ω)")
+    ax.loglog(
+        ib_exp[i][100:-1],
+        rb_exp[i][100:],
+        ["r.", "g.", "b."][i],
+        label="Measured incremental base resistance (" + rnames[i] + " Ω)",
+    )
+    ax.plot(
+        ib_exp[i][100:-1],
+        rb_c[i][100:],
+        ["k--", "k-.", "k-"][i],
+        label="Theoretical incremental base resistance (" + rnames[i] + "Ω)",
+    )
 
 plt.title("Emitter-Degenerated Incremental Base Resistance")
 plt.xlabel("Base current (A)")
@@ -177,8 +226,18 @@ ax.cla()
 
 # Joined log-log transconductance plot
 for i in range(3):
-  ax.loglog(ib_exp[i][100:-1], gm_exp[i][100:], ['r.', 'g.', 'b.'][i], label="Measured inc. trans. (" + rnames[i] + " Ω)")
-  ax.plot(ib_exp[i][100:-1], gm_c[i][100:], ['k--', 'k-.', 'k-'][i], label="Theoretical inc. trans. (" + rnames[i] + "Ω)")
+    ax.loglog(
+        ib_exp[i][100:-1],
+        gm_exp[i][100:],
+        ["r.", "g.", "b."][i],
+        label="Measured inc. trans. (" + rnames[i] + " Ω)",
+    )
+    ax.plot(
+        ib_exp[i][100:-1],
+        gm_c[i][100:],
+        ["k--", "k-.", "k-"][i],
+        label="Theoretical inc. trans. (" + rnames[i] + "Ω)",
+    )
 
 plt.title("Emitter-Degenerated Incremental Base-Collector Transconductance")
 plt.xlabel("Base current (A)")
