94fb4b84d7633853121a5aee8c0a522bbcf79aa2
Adam Selker <adamselker@gmail.com>
2019-04-30 22:49:20 -0400

    plot exp4 as points

--- exp4_transfer_plot.py
+++ exp4_transfer_plot.py
@@ -6,29 +6,29 @@ from numpy.polynomial.polynomial import polyfit
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-vals = ["exp3_10k","exp4_20k", "exp4_30k", "exp4_40k"]
+vals = ["exp3_10k", "exp4_20k", "exp4_30k", "exp4_40k"]
 
 vb_exp = {}
 ib_exp = {}
 ve_exp = {}
 
 for val in vals:
-  with open("trans_%s.csv" % val) as f:
-    c = csv.reader(f, delimiter=",")
-    next(c) # Throw away the header
-    #for _ in range(400):
-    #  next(c) # Throw away bad data points
-    vb = []
-    ib = []
-    ve = []
-    for row in c:
-      vb += [float(row[0])]
-      ib += [float(row[1])]
-      ve += [float(row[2])]
+    with open("trans_%s.csv" % val) as f:
+        c = csv.reader(f, delimiter=",")
+        next(c)  # Throw away the header
+        # for _ in range(400):
+        #  next(c) # Throw away bad data points
+        vb = []
+        ib = []
+        ve = []
+        for row in c:
+            vb += [float(row[0])]
+            ib += [float(row[1])]
+            ve += [float(row[2])]
 
-    vb_exp[val] = vb
-    ib_exp[val] = ib
-    ve_exp[val] = ve
+        vb_exp[val] = vb
+        ib_exp[val] = ib
+        ve_exp[val] = ve
 
 valid = {}
 valid[vals[0]] = (100, 960)
@@ -36,27 +36,47 @@ valid[vals[1]] = (200, 400)
 valid[vals[2]] = (200, 300)
 valid[vals[3]] = (200, 300)
 
-params = curve_fit(lambda Vb, Av, b: Vb*Av+b, vb_exp[vals[0]][valid[vals[0]][0]:valid[vals[0]][1]], ve_exp[vals[0]][valid[vals[0]][0]:valid[vals[0]][1]])
+params = curve_fit(
+    lambda Vb, Av, b: Vb * Av + b,
+    vb_exp[vals[0]][valid[vals[0]][0] : valid[vals[0]][1]],
+    ve_exp[vals[0]][valid[vals[0]][0] : valid[vals[0]][1]],
+)
 Av, b = params[0][0], params[0][1]
 
 
 fig = plt.figure()
 ax = plt.subplot(111)
-ax.plot(vb_exp[vals[0]], ve_exp[vals[0]], '.', label="Measured Emitter Voltage")
-ax.plot(vb_exp[vals[0]], np.array(vb_exp[vals[0]])*Av+b, 'k-', label="Theoretical Fit no ₘR (Aᵥ = %.2f, V₀ = %.2f)" % (Av,b))
+ax.plot(vb_exp[vals[0]], ve_exp[vals[0]], ".", label="Measured Emitter Voltage")
+ax.plot(
+    vb_exp[vals[0]],
+    np.array(vb_exp[vals[0]]) * Av + b,
+    "k-",
+    label="Theoretical Fit no ₘR (Aᵥ = %.2f, V₀ = %.2f)" % (Av, b),
+)
 
 
-for i in range(1,4):
-  ax.plot(vb_exp[vals[i]],ve_exp[vals[i]], label="ₘR = " + vals[i][5:])
-  params = curve_fit(lambda Vb, Av, b: Vb*Av+b, vb_exp[vals[i]][valid[vals[i]][0]:valid[vals[i]][1]], ve_exp[vals[i]][valid[vals[i]][0]:valid[vals[i]][1]])
-  Av, b = params[0][0], params[0][1]
-  ax.plot(vb_exp[vals[i]], np.array(vb_exp[vals[i]])*Av+b, 'k-', label="Theoretical Fit %s (Aᵥ = %.2f, V₀ = %.2f)" % (vals[i][5:],Av,b))
+for i in range(1, 4):
+    ax.plot(
+        vb_exp[vals[i]], ve_exp[vals[i]], ".", label="ₘR = " + vals[i][5:], markersize=2
+    )
+    params = curve_fit(
+        lambda Vb, Av, b: Vb * Av + b,
+        vb_exp[vals[i]][valid[vals[i]][0] : valid[vals[i]][1]],
+        ve_exp[vals[i]][valid[vals[i]][0] : valid[vals[i]][1]],
+    )
+    Av, b = params[0][0], params[0][1]
+    ax.plot(
+        vb_exp[vals[i]],
+        np.array(vb_exp[vals[i]]) * Av + b,
+        "k-",
+        label="Theoretical Fit %s (Aᵥ = %.2f, V₀ = %.2f)" % (vals[i][5:], Av, b),
+    )
 
 plt.title("Voltage Transfer Characteristic (VTC)")
 plt.xlabel("Base Voltage (V)")
 plt.ylabel("Emitter Voltage (V)")
 plt.grid(True)
-plt.ylim(0,6)
+plt.ylim(0, 6)
 ax.legend()
-#plt.show()
+# plt.show()
 plt.savefig("theoretical_exp4.pdf")
